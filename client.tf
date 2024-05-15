9379baf144247f5c3f75d8e50e6764ce25ae6cc7
Zakariyah Abdel-Illah <zak@mqno.dev>
2024-05-15 13:40:41 -0400

    pki: expose server / client keys

--- client.tf
+++ client.tf
@@ -1,36 +1,36 @@
 variable "clients" {
-  type = map
+  type        = map(any)
   description = "keys of machines to generate certificates for, values of the certificate subject"
-  default = {}
+  default     = {}
 }
 
 variable "default_client_subject" {
-  type = map
+  type        = map(any)
   description = "fallback values for certificate subjects, should the value of a server be null"
-  default = {}
+  default     = {}
 }
 
 variable "client_csrs" {
-  type = map
+  type        = map(any)
   description = "csrs to use instead of generating them within this module"
-  default = {}
+  default     = {}
 }
 
 variable "client_certificate_validity" {
-  type = number
-  default = 43800
+  type        = number
+  default     = 43800
   description = "length of the validity of the certificate"
 }
 
 resource "tls_private_key" "pem_client" {
-  for_each = var.clients
+  for_each  = var.clients
   algorithm = var.algorithm
 }
 
 resource "tls_cert_request" "csr_client" {
-  for_each = var.clients
+  for_each        = var.clients
   private_key_pem = tls_private_key.pem_client[each.key].private_key_pem
-  dns_names = [each.key]
+  dns_names       = [each.key]
 
   subject {
     country             = try(each.value.country, try(var.default_client_subject.country, var.default_subject.country))
@@ -43,10 +43,10 @@ resource "tls_cert_request" "csr_client" {
 }
 
 resource "tls_locally_signed_cert" "client" {
-  for_each = var.clients
-  cert_request_pem = tls_cert_request.csr_client[each.key].cert_request_pem
+  for_each           = var.clients
+  cert_request_pem   = tls_cert_request.csr_client[each.key].cert_request_pem
   ca_private_key_pem = tls_private_key.pem_ca.private_key_pem
-  ca_cert_pem = tls_self_signed_cert.ca.cert_pem
+  ca_cert_pem        = tls_self_signed_cert.ca.cert_pem
 
   validity_period_hours = var.client_certificate_validity
 
@@ -59,22 +59,28 @@ resource "tls_locally_signed_cert" "client" {
 }
 
 resource "pkcs12_from_pem" "client" {
-  for_each = var.clients
+  for_each        = var.clients
   ca_pem          = tls_self_signed_cert.ca.cert_pem
   cert_pem        = tls_locally_signed_cert.client[each.key].cert_pem
   private_key_pem = tls_private_key.pem_client[each.key].private_key_pem
-  password = "123" # Testing purposes
-  encoding = "legacyRC2"
+  password        = "123" # Testing purposes
+  encoding        = "legacyRC2"
+}
+
+output "client_keys" {
+  value       = [for cert in tls_locally_signed_cert.client : cert.private_key_pem]
+  description = "generated client certificates in ordered list form"
+  sensitive   = true
 }
 
 output "client_certificates" {
-  value = [ for cert in tls_locally_signed_cert.client : cert.cert_pem ]
+  value       = [for cert in tls_locally_signed_cert.client : cert.cert_pem]
   description = "generated client certificates in ordered list form"
-  sensitive = true
+  sensitive   = true
 }
 
 output "client_certificates_pkcs12" {
-  value = [ for cert in pkcs12_from_pem.client : cert.result ]
+  value       = [for cert in pkcs12_from_pem.client : cert.result]
   description = "combined ca certificate, private key and ca certificate in pkcs12 format"
-  sensitive = true
+  sensitive   = true
 }
--- servers.tf
+++ servers.tf
@@ -1,36 +1,36 @@
 variable "servers" {
-  type = map
+  type        = map(any)
   description = "keys of machines to generate certificates for, values of the certificate subject"
-  default = {}
+  default     = {}
 }
 
 variable "default_server_subject" {
-  type = map
+  type        = map(any)
   description = "fallback values for certificate subjects, should the value of a server be null"
-  default = {}
+  default     = {}
 }
 
 variable "server_csrs" {
-  type = map
+  type        = map(any)
   description = "csrs to use instead of generating them within this module"
-  default = {}
+  default     = {}
 }
 
 variable "server_certificate_validity" {
-  type = number
-  default = 43800
+  type        = number
+  default     = 43800
   description = "length of the validity of the certificate"
 }
 
 resource "tls_private_key" "pem_server" {
-  for_each = var.servers
+  for_each  = var.servers
   algorithm = var.algorithm
 }
 
 resource "tls_cert_request" "csr_server" {
-  for_each = var.servers
+  for_each        = var.servers
   private_key_pem = tls_private_key.pem_server[each.key].private_key_pem
-  dns_names = [each.key]
+  dns_names       = [each.key]
 
   subject {
     country             = try(each.value.country, try(var.default_server_subject.country, var.default_subject.country))
@@ -43,10 +43,10 @@ resource "tls_cert_request" "csr_server" {
 }
 
 resource "tls_locally_signed_cert" "server" {
-  for_each = var.servers
-  cert_request_pem = tls_cert_request.csr_server[each.key].cert_request_pem
+  for_each           = var.servers
+  cert_request_pem   = tls_cert_request.csr_server[each.key].cert_request_pem
   ca_private_key_pem = tls_private_key.pem_ca.private_key_pem
-  ca_cert_pem = tls_self_signed_cert.ca.cert_pem
+  ca_cert_pem        = tls_self_signed_cert.ca.cert_pem
 
   validity_period_hours = var.client_certificate_validity
 
@@ -59,23 +59,28 @@ resource "tls_locally_signed_cert" "server" {
 }
 
 resource "pkcs12_from_pem" "server" {
-  for_each = var.servers
+  for_each        = var.servers
   ca_pem          = tls_self_signed_cert.ca.cert_pem
   cert_pem        = tls_locally_signed_cert.server[each.key].cert_pem
   private_key_pem = tls_private_key.pem_server[each.key].private_key_pem
-  password = "123" # Testing purposes
-  encoding = "legacyRC2"
+  password        = "123" # Testing purposes
+  encoding        = "legacyRC2"
 }
 
+output "server_keys" {
+  value       = [for cert in tls_locally_signed_cert.server : cert.private_key_pem]
+  description = "generated server certificate"
+  sensitive   = true
+}
 
 output "server_certificates" {
-  value = [ for cert in tls_locally_signed_cert.server : cert.cert_pem ]
+  value       = [for cert in tls_locally_signed_cert.server : cert.cert_pem]
   description = "generated server certificate"
-  sensitive = true
+  sensitive   = true
 }
 
 output "server_certificates_pkcs12" {
-  value = [ for cert in pkcs12_from_pem.server : cert.result ]
+  value       = [for cert in pkcs12_from_pem.server : cert.result]
   description = "combined ca certificate, private key and ca certificate in pkcs12 format"
-  sensitive = true
+  sensitive   = true
 }
